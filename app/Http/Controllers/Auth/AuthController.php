5a22a3ac1b45166e69ebb8203fbcab6b17216ecb
Harry <harry_mercury@hotmail.co.uk>
2016-08-16 16:58:34 +0100

    tiny auth change

--- app/Http/Controllers/Auth/AuthController.php
+++ app/Http/Controllers/Auth/AuthController.php
@@ -68,7 +68,7 @@ class AuthController extends Controller
             'name' => $data['name'],
             'email' => $data['email'],
             'password' => bcrypt($data['password']),
-            'is_admin' => true,
+            'is_admin' => 1,
         ]);
     }
 
